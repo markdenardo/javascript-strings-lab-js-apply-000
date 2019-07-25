00a01ab76aa55af9817c52590bee2df60f13e9ce
Mark DeNardo <mark.denardo@gmail.com>
2019-07-25 03:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Ler's talk about";
+var topic = "space";
 
 var conversation = `${topic}`;
